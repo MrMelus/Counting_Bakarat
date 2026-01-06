45232e548544c3c62ec7655c72e219a1e16f4eea
Carmelo Ferrara <carmelof437@gmail.com>
2026-01-06 16:50:09 +0100

    fix + feat

--- fullGameSimulator.py
+++ fullGameSimulator.py
@@ -1,15 +1,35 @@
+import random
+
+
 
 def deck_composition(n):
-    deck = [0] *13
-    cards = 52*8
+    game_state = {
+        "deck": [0] * 13,
+        "cards": 52 * 8,
+        "p1": 0,
+        "p2": 0,
+        "banco": 0
+    }
+    for i in range(n):
+        play(game_state)
+
+def play(gs):
+    while gs["cards"]>9:
+        deal(gs) 
+
+def deal(gs):
+    value = pick(gs) +1
+    gs["cards"] -=1
+    if value >= 10: value = 0
     
-
     
-
-
-
-
-
+def pick(gs):
+    value = random.randint(0,12)
+    while gs["deck"][value]>31:
+        value = random.randint(0,12)
+    gs["deck"][value] += 1
+    return value
+    
 def main():
     n = int(input("Quante volte vuoi simulare?\n"))
     deck_composition(n)
