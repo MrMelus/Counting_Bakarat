e6ad090463e5fb5262e543b4f8b83b8e2375627e
Carmelo Ferrara <carmelof437@gmail.com>
2026-01-14 14:07:08 +0100

    ref: aggiunto betValue nel game_state

--- fullGameSimulator.py
+++ fullGameSimulator.py
@@ -13,6 +13,7 @@ def deck_composition(n,nome_strategia):
         "p1": 0,
         "p2": 0,
         "banco": 0,
+        "betValue": 0,
         "balance" : 0
     }
     for i in range(n):
@@ -28,17 +29,17 @@ def deck_composition(n,nome_strategia):
     df = pd.DataFrame(report_giocate)
     nome_file = f"report_metodi/risultati_{nome_strategia}.xlsx"
     df.to_excel(nome_file,index= False)
-    
+
 
 def play(gs):
     while gs["cards"]>9:
         gs["p1"] = 0
         gs["p2"] = 0
         gs["banco"] = 0
-        betValue = 10
+        gs["betValue"] = 10
         deal(gs) 
         #print(gs["n"],") ",end="")
-        winners(gs,betValue)
+        winners(gs)
         gs["n"] += 1
     print(gs["balance"], end= " ")
 
@@ -98,14 +99,14 @@ def banco_playstyle(gs,c1,c2):
     elif gs["banco"] <=4: return 1
     else: return 0
 
-def winners(gs,betValue):
+def winners(gs):
     # Determiniamo lo stato di p1 rispetto al banco
     res1 = "vince" if gs["p1"] > gs["banco"] else ("come" if gs["p1"] == gs["banco"] else "perde")
     # Determiniamo lo stato di p2 rispetto al banco
     res2 = "vince" if gs["p2"] > gs["banco"] else ("come" if gs["p2"] == gs["banco"] else "perde")
     
-    if res1 == "vince": gs["balance"] += betValue
-    elif res1 == "perde": gs["balance"] -= betValue
+    if res1 == "vince": gs["balance"] += gs["betValue"]
+    elif res1 == "perde": gs["balance"] -= gs["betValue"]
 
     #print(f"P1 {res1}, P2 {res2} | Banco aveva: {gs['banco']}")
 
